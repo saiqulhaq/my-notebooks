5f30cc5015bb11810232ade85d099fe1b63a4799
Saiqul Haq <saiqulhaq@gmail.com>
2023-08-22 22:05:43 +0700

    update docker image

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-docker run -it -p 8888:8888 --env-file .env -v "${PWD}":/home/jovyan/work --user 1000 --group-add users --rm -e RESTARTABLE=yes my-notebooks
+docker run -it -p 8888:8888 --env-file .env -v "${PWD}":/home/jovyan/work --user 1000 --group-add users --rm -e RESTARTABLE=yes  rubydata/datascience-notebook
