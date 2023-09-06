058f8edc7e514dec75c6603eba96bce3d344885a
Saiqul Haq <saiqulhaq@gmail.com>
2023-09-06 08:42:52 +0700

    learn ruby

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-docker run -it -p 8888:8888 --env-file .env -v "${PWD}":/home/jovyan/work --user 1000 --group-add users --rm -e RESTARTABLE=yes  rubydata/datascience-notebook
+docker run -p 8888:8888 -v /home/chucky/projects/github.com/saiqulhaq/my-notebooks:/notebooks --gpus all ankane/ml-stack:standard
